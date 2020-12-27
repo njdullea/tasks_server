9caa4005f69a49cd7d9a638d79d90de1e855fe0d
Nathan Dullea <njdullea@gmail.com>
2020-12-26 18:41:09 -0700

    public ip address

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,8 @@ async fn main() -> std::io::Result<()> {
             .app_data(counter.clone())
             .route("/", web::get().to(index))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
+    // .bind("127.0.0.1:8080")?
     .run()
     .await
 }
